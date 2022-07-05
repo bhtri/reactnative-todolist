d2703180c72537ca522059753feb4bc3da9c1029
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-05 23:51:41 +0900

    ⚡️ Style item 01

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <Text style={styles.header}>Todo List</Text>
+        <View style={styles.item}>
+          <View style={styles.square}>
+            <Text style={styles.number}>01</Text>
+          </View>
+          <Text style={styles.content}>Lau nha</Text>
+        </View>
+        <View style={styles.item}>
+          <View style={styles.square}>
+            <Text style={styles.number}>02</Text>
+          </View>
+          <Text style={styles.content}>Rua chen</Text>
+        </View>
+      </View>
+      <View style={styles.input}></View>
     </View>
   );
 }
@@ -13,8 +26,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#eff7f8',
+  },
+  body: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 18,
+  },
+  header: {
+    fontSize: 24,
+    color: '#21a3d8',
+    fontWeight: 'bold'
+  },
+  item: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
+    marginBottom: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  square: {
+    width: 48,
+    height: 36,
+    borderRadius: 10,
+    backgroundColor: '#53d6f2',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  number: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#fff'
+  },
+  content: {
+    width: '80%',
+    fontSize: 16,
   },
 });

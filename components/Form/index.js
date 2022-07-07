c3dad2aabbf4d8ba9fa5790e8df2c78040820ab3
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-07-08 01:47:13 +0900

    ⚡️ Style input 02

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,15 +1,20 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
 
 import styles from './style'
 
 export default function Form() {
   return (
-    <View style={styles.addTask}>
-      <TextInput style={styles.input} />
-      <View style={styles.iconWrapper}>
-        <Text style={styles.icon}>+</Text>
-      </View>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset = {10}
+      style={styles.addTask}>
+      <TextInput placeholder='Your Task' style={styles.input} />
+      <TouchableOpacity>
+        <View style={styles.iconWrapper}>
+          <Text style={styles.icon}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   )
 }
